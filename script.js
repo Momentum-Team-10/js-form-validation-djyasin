9f900de20dfafcacf061714f5b755f5faf85bff8
Lila <djyasin.ly@gmail.com>
2021-10-11 18:59:04 -0400

    addeed let statements and event listeners. trying to get name function to work, but no luck thus far.

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
-let root = document.getElementById('root')
+let form = document.getElementById("parking-form");
+let nameInput = document.getElementById('name');
+let carYear = document.getElementById('car-year');
+let stayLength = document.getElementById('days');
+let cvv = document.getElementById('cvv');
+let submitButton = document.getElementById('submit-button');
+let carMake = document.getElementById('car-make');
+let modelType = document.getElementById('car-model');
+let creditCard = document.getElementById('credit-card');
+
+
 
 console.log('Add validation!');
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+//    validateName(nameInput);
+//    validateCarYear(carYear);
+//    validatestayLength(stayLength);
+//    validateMake(carMake);
+//    validateModel(modelType);
+//   validateCC(creditCard);
+//   validateCVV(cvv);
+});
+
+function validateName(input) {
+    let errorEl = document.createElement('p')
+    errorName.id = "name-error";
+ 
+    if ((input.value) !== '') {
+        errorName.innerText = "Please enter your name."
+        document.getElementById('input-field').appendChild(errorEl)
+        console.log(' enter name')
+        nameInput.classList.add('invalid')
+    } else {
+        if (document.getElementById('name-error')) {
+            document.getElementById('name-error').remove()
+        }
+        nameInput.classList.remove('invalid')
+        nameInput.classList.add('valid')
+}}
