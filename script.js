fc99df8a6b434b84fdb5c55eb2789686311efcd2
Lila <djyasin.ly@gmail.com>
2021-10-11 21:25:13 -0400

    Not getting error message on name yet, but fixed naming of css and got red green function to work.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let form = document.getElementById("parking-form");
-let nameInput = document.getElementById('name');
+let nameInput = document.getElementById('name-field');
 let carYear = document.getElementById('car-year');
 let stayLength = document.getElementById('days');
 let cvv = document.getElementById('cvv');
@@ -14,7 +14,7 @@ console.log('Add validation!');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-//    validateName(nameInput);
+   validateName(nameInput);
 //    validateCarYear(carYear);
 //    validatestayLength(stayLength);
 //    validateMake(carMake);
@@ -23,14 +23,14 @@ form.addEventListener('submit', (event) => {
 //   validateCVV(cvv);
 });
 
-function validateName(input) {
-    let errorEl = document.createElement('p')
+function validateName (input) {
+    let errorName = document.createElement('p');
     errorName.id = "name-error";
  
     if ((input.value) !== '') {
-        errorName.innerText = "Please enter your name."
-        document.getElementById('input-field').appendChild(errorEl)
-        console.log(' enter name')
+ //       errorName.innerText = "Please enter your name."
+  //      document.getElementById('name-error').appendChild(errorName)
+  //      console.log('enter name')
         nameInput.classList.add('invalid')
     } else {
         if (document.getElementById('name-error')) {
@@ -39,3 +39,24 @@ function validateName(input) {
         nameInput.classList.remove('invalid')
         nameInput.classList.add('valid')
 }}
+
+//function validateName(input) {
+ //   let errorName = document.createElement('p');
+ //   errorName.id = 'error-Name';
+
+ //   if (input.value !== '') {
+ //       console.log('test passed!');
+ //       input.classList.remove('invalid');
+  //      input.classList.add('valid');
+ //       document.getElementById('error-Name').remove();
+  //  } else {
+ //       if (document.getElementById('error-Name')) {
+ //           document.getElementById('error-Name').remove();
+ //       }
+
+ //       errorName.innerText = 'This is a required field';
+ //       document.getElementById('name-field').appendChild(errorName);
+ //       input.classList.remove('valid');
+ //       input.classList.add('invalid');
+  //  }
+//x}
